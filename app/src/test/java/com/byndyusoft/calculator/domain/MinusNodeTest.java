04def39e71577005cbbb5842901c450fa0c47871
Fedor Sherstobitov <fsherstobitov@gmail.com>
2017-02-23 22:06:38 +0500

    Add MinusNode with tests

---
+++ app/src/test/java/com/byndyusoft/calculator/domain/MinusNodeTest.java
@@ -0,0 +1,24 @@
+package com.byndyusoft.calculator.domain;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+public class MinusNodeTest {
+
+    @Test
+    public void testEval() {
+        AbstractNode node = new MinusNode(new NumberNode(10), new NumberNode(5));
+        double value = node.eval();
+
+        assertEquals(5, value, 0.0);
+    }
+
+    @Test
+    public void testEvalNegative() {
+        AbstractNode node = new MinusNode(new NumberNode(3), new NumberNode(8));
+        double value = node.eval();
+
+        assertEquals(-5, value, 0.0);
+    }
+}
