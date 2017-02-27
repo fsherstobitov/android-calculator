69df28640ab57d1ad0138f460a5ba0d793c15957
Fedor Sherstobitov <fsherstobitov@gmail.com>
2017-02-27 18:23:18 +0500

    Add implementation of CalculatorView.setValid in MainActivity

--- app/src/main/java/com/byndyusoft/calculator/MainActivity.java
+++ app/src/main/java/com/byndyusoft/calculator/MainActivity.java
@@ -1,11 +1,13 @@
 package com.byndyusoft.calculator;
 
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.byndyusoft.calculator.domain.parser.SimpleExpressionValidator;
 import com.byndyusoft.calculator.domain.parser.SimpleParser;
 
 public class MainActivity extends AppCompatActivity implements CalculatorView {
@@ -20,7 +22,7 @@ public class MainActivity extends AppCompatActivity implements CalculatorView {
         setContentView(R.layout.activity_main);
         screen = (TextView) findViewById(R.id.screen);
 
-        presenter = new CalculatorPresenter(this, new SimpleParser());
+        presenter = new CalculatorPresenter(this, new SimpleParser(), new SimpleExpressionValidator());
     }
 
     @Override
@@ -40,5 +42,14 @@ public class MainActivity extends AppCompatActivity implements CalculatorView {
     @Override
     public void updateScreen(String content) {
         screen.setText(content);
+    }
+
+    @Override
+    public void setValid(boolean isValid) {
+        if (isValid) {
+            screen.setBackgroundColor(ContextCompat.getColor(this, R.color.colorValidationPassed));
+        } else {
+            screen.setBackgroundColor(ContextCompat.getColor(this, R.color.colorValidationFailed));
+        }
     }
 }
