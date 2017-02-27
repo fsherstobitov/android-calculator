c0e78ecf625ce4460c83f0284041779136854c03
Fedor Sherstobitov <fsherstobitov@gmail.com>
2017-02-27 13:58:25 +0500

    Implement CalculatorView contract in MainActivity

--- app/src/main/java/com/byndyusoft/calculator/MainActivity.java
+++ app/src/main/java/com/byndyusoft/calculator/MainActivity.java
@@ -2,12 +2,43 @@ package com.byndyusoft.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.byndyusoft.calculator.domain.parser.SimpleParser;
+
+public class MainActivity extends AppCompatActivity implements CalculatorView {
+
+    private TextView screen;
+
+    private CalculatorPresenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        screen = (TextView) findViewById(R.id.screen);
+
+        presenter = new CalculatorPresenter(this, new SimpleParser());
+    }
+
+    @Override
+    protected void onDestroy() {
+        presenter.onDestroy();
+        super.onDestroy();
+    }
+
+    public void onButtonClick(View button) {
+        presenter.onButtonClick(((Button) button).getText().toString());
+    }
+
+    public void onEqualsClick(View equalsButton) {
+        presenter.onEquals();
+    }
+
+    @Override
+    public void updateScreen(String content) {
+        screen.setText(content);
     }
 }
